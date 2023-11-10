85f7f0c5af0395f01161d4dd1b3140b265432794
kurivill <kurikkaville@hotmail.fi>
2023-11-10 13:52:52 +0200

    5/10

---
+++ assingment1/tehtävä5/5.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const vuosiluku = +prompt(`Anna vuosiluku`)
+
+
+if (vuosiluku % 4 === 0) {
+  if (vuosiluku % 100 !== 0) {
+    document.querySelector("#vastaus").innerHTML = `Vuosi on karkausvuosi`
+  }
+  else if (vuosiluku % 100 === 0 && vuosiluku % 400 === 0) {
+    document.querySelector("#vastaus").innerHTML = `Vuosi on karkausvuosi`
+  }
+  else {
+    document.querySelector("#vastaus").innerHTML = `Vuosi ei ole karkausvuosi`
+  }
+}
+else {
+  document.querySelector("#vastaus").innerHTML = `Vuosi ei ole karkausvuosi`
+}
+
