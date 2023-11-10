4e47c120ece64b50514e8813b5cb0bee168a518b
kurivill <kurikkaville@hotmail.fi>
2023-11-10 14:33:47 +0200

    6/10

--- assingment1/tehtävä7/7.js
+++ assingment1/tehtävä7/7.js
@@ -4,6 +4,8 @@ const heittolkm = +prompt("How many dice should I throw?");
 let kirjanpito = 0;
 let summa = 0;
 
+const kohde = document.querySelector("#vastaus7");
+
 
 while (kirjanpito != heittolkm) {
   let heitto = Math.floor(Math.random() * 6 + 1);
@@ -13,7 +15,7 @@ while (kirjanpito != heittolkm) {
 
 }
 
-console.log(summa);
+kohde.innerHTML = `The sum of throws is ${summa}`
 
 
 
