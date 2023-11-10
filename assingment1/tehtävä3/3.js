945bd4f346f80a4b1b2295f8c96767f5a0c0961b
kurivill <kurikkaville@hotmail.fi>
2023-11-10 13:21:37 +0200

    3/10

--- assingment1/tehtävä3/3.js
+++ assingment1/tehtävä3/3.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const eka = +prompt("Give the first number");
+const toka = +prompt("Give the second number");
+const kolmas = +prompt("Give the third number");
+
+const vastauskenttä = document.querySelector("#printti");
+vastauskenttä.innerHTML = `Sum of given numbers: ${eka + toka + kolmas}<br>`;
+vastauskenttä.innerHTML += `Product of given numbers: ${eka * toka * kolmas}<br>`;
+vastauskenttä.innerHTML += `Average of given numbers: ${(eka + toka + kolmas) / 3}`;
